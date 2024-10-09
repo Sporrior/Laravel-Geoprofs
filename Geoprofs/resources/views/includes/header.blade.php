1d7b30f31825bbe4eef884a4e14aff6d536e01d3
Wessam N <27088282+GoldenMan020@users.noreply.github.com>
2024-10-09 10:14:41 -0400

    commit end of the day
    header aangepast dropdown menu

--- Geoprofs/resources/views/includes/header.blade.php
+++ Geoprofs/resources/views/includes/header.blade.php
@@ -6,12 +6,13 @@
     }
 
     .nav-profiel-foto {
-        width: 40%;
-        margin-left: 25%;
+        width: 30%;
+        margin-left: 10%;
     }
 
-    .navbar-right{
-        width: 10%;
+    .navbar-right {
+        width: 30%;
+        display: flex;
     }
 
     .navbar {
@@ -71,7 +72,101 @@
         object-fit: cover;
         border: 2px solid #ddd;
     }
+    /* profile menu */
+
+    .profiel-dropdown {
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        /* align-content: end; */
+        gap: 12px;
+    }
+
+    .profiel-dropdown h3 {
+        text-align: end;
+        line-height: 1;
+        font-weight: 600;
+    }
+
+    .profiel-dropdown p {
+        line-height: 1;
+        font-size: 14px;
+        opacity: .6;
+    }
+
+    .profiel-dropdown .img-box {
+        position: relative;
+        width: 48px;
+        height: 48px;
+        border-radius: 50%;
+        overflow: hidden;
+    }
+
+    .profiel-dropdown .img-box img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    /* menu (the right one) */
+
+    .menu {
+        position: absolute;
+        top: calc(100% + 10px);
+        right: 0px;
+        width: 200px;
+        min-height: 100px;
+        background: #fff;
+        box-shadow: 0 10px 20px rgba(0, 0, 0, .2);
+        opacity: 0;
+        transform: translateY(-10px);
+        visibility: hidden;
+        transition: 300ms;,transform: 300ms;
+    }
+
+
+    .menu.active {
+        opacity: 1;
+        transform: translateY(0);
+        visibility: visible;
+    }
+
+    /* menu links */
+
+    .menu ul {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        z-index: 10;
+        background: #fff;
+    }
+
+    .menu ul li {
+        list-style: none;
+    }
+
+    .menu ul li:hover {
+        background: #eee;
+    }
+
+    .menu ul li a {
+        text-decoration: none;
+        color: #000;
+        display: flex;
+        align-items: center;
+        padding: 15px 20px;
+        gap: 6px;
+    }
+
+    .menu ul li a i {
+        font-size: 1.2em;
+    }
 </style>
+
+
 
 
 <nav class="navbar">
@@ -85,15 +180,34 @@
     </div>
 
     <div class="navbar-center">
-        <a href="#" class="nav-link active">Verlof</a>
+        <a href="#" class="nav-link ">Verlof</a>
         <a href="#" class="nav-link">Ziekmelden</a>
         <a href="#" class="nav-link">Goedkeuring</a>
     </div>
 
     <div class="navbar-right">
-        <a href="{{ route('profiel.show') }}">
+        <div class="profiel-dropdown">
+            <h3>Katherine Cooper</h3>
+        </div>
+        <div class="img-box">
             <img id="profielFotoDisplay" src="{{ asset('storage/' . $user->profielFoto) }}" alt="Gebruikersprofiel"
                 class="nav-profiel-foto">
-        </a>
+        </div>
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="#"><i class="ph-bold ph-user"></i>Profile</a></li>
+            <li><a href="#"><i class="ph-bold ph-gear-six"></i>Settings</a></li>
+            <li><a href="#"><i class="ph-bold ph-sign-out"></i>Sign Out</a></li>
+        </ul>
     </div>
 </nav>
+
+<script>
+    let profielDropdown = document.querySelector('.profiel-dropdown');
+    let menu = document.querySelector('.menu');
+
+    profielDropdown.onclick = function () {
+        menu.classList.toggle('active');
+    }
+</script>
