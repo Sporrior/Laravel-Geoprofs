43b7e1e0b5c9d41e76e052abb3ed35387743258b
Wessam N <27088282+GoldenMan020@users.noreply.github.com>
2024-11-04 06:07:44 -0500

    Update keuring.blade.php

--- Geoprofs/resources/views/keuring.blade.php
+++ Geoprofs/resources/views/keuring.blade.php
@@ -1,3 +1,8 @@
+@if($user->role->id == 2 || $user->role->id == 3)
+    {{ 'Page' }}
+@elseif($user->role->id == 1)
+    <script>window.location = "/dashboard";</script>
+@endif
 <!DOCTYPE html>
 <html lang="en">
 
