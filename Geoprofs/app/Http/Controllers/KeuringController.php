7eb6830551abeba38b8c8dd72d271068dab6136b
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2024-11-04 11:22:54 +0100

    verlof goedkeuren dan gaat vakanitie dagen min

--- Geoprofs/app/Http/Controllers/KeuringController.php
+++ Geoprofs/app/Http/Controllers/KeuringController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\VerlofAanvragen;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class KeuringController extends Controller
 {
-
     public function __construct()
     {
         $this->middleware('auth');
@@ -16,7 +17,7 @@ class KeuringController extends Controller
 
     public function index()
     {
-        // Fetch all leave requests (verlofaanvragens)
+        // Fetch all leave requests (verlofaanvragens) with related user and type
         $verlofaanvragens = VerlofAanvragen::with('user', 'type')->get();
 
         // Pass them to the view
@@ -28,8 +29,28 @@ class KeuringController extends Controller
         // Find the leave request by ID
         $verlofAanvraag = VerlofAanvragen::findOrFail($id);
 
-        // Update the status based on the user's selection
-        $verlofAanvraag->status = $request->input('status');
+        // Retrieve the new status from the request
+        $status = $request->input('status');
+
+        // Check if the new status is 'approved' (1) and if it wasn't approved before
+        if ($status == 1 && $verlofAanvraag->status !== 1) {
+            // Convert start and end dates to Carbon instances
+            $startDatum = Carbon::parse($verlofAanvraag->start_datum);
+            $eindDatum = Carbon::parse($verlofAanvraag->eind_datum);
+
+            // Calculate the number of requested days
+            $requestedDays = $startDatum->diffInDays($eindDatum) + 1;
+
+            // Retrieve the user associated with the leave request
+            $user = $verlofAanvraag->user;
+
+            // Subtract the requested days from the user's available leave days
+            $user->verlof_dagen -= $requestedDays;
+            $user->save();
+        }
+
+        // Update the status on the leave request
+        $verlofAanvraag->status = $status;
         $verlofAanvraag->save();
 
         // Redirect back to the keuring page with a success message
