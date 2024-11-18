da71b1fb328edd0154f75b3e62fcb6a4b59a1808
Damien <120579307+Sporrior@users.noreply.github.com>
2024-11-18 12:09:06 +0100

    push

--- Geoprofs/generate-token.php
+++ Geoprofs/generate-token.php
@@ -1,3 +1,5 @@
 <?php
 $token = bin2hex(random_bytes(32)); // Generates a 64-character token
-echo $token . PHP_EOL;
+file_put_contents('token.txt', $token); // Saves the token in a text file!!
+echo "Token generated and saved to token.txt\n";
+?>
