954abb514b0bb55e6418a20e7e9c651e5a883b4f
Damien <120579307+Sporrior@users.noreply.github.com>
2024-11-20 10:27:24 +0100

    pushvoor api

--- Geoprofs/README.md
+++ Geoprofs/README.md
@@ -1,3 +1,3 @@
-use `php artisan serve --host=0.0.0.0 host=0.0.0.0 --port=8000` to start the server.
+use `php artisan serve --host=0.0.0.0 --port=8000` to start the server.
 
 then run `ngrok http 8000`
--- Geoprofs/generate-token.php
+++ Geoprofs/generate-token.php
@@ -1,5 +1,5 @@
 <?php
-$token = bin2hex(random_bytes(32)); // Generates a 64-character token
-file_put_contents('token.txt', $token . PHP_EOL, FILE_APPEND); // Appends the token to the file with a newline
+$token = bin2hex(random_bytes(32));
+file_put_contents('token.txt', $token . PHP_EOL, FILE_APPEND);
 echo "Token generated and saved to token.txt\n";
 ?>
