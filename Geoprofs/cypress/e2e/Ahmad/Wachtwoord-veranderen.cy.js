ba89337b0b4f218c324bd4c2f6199496cc0546b2
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2025-01-10 10:55:06 +0100

    cypress

--- Geoprofs/cypress/e2e/Ahmad/Wachtwoord-veranderen.cy.js
+++ Geoprofs/cypress/e2e/Ahmad/Wachtwoord-veranderen.cy.js
@@ -15,5 +15,26 @@ describe('Login, Navigate to Settings, and Change password', () => {
       cy.visit('http://localhost:8000/profiel');
 
 
+      cy.get('#huidigWachtwoord').type('User01', { log: false });
+      cy.get('#nieuwWachtwoord').type('User010', { log: false });
+      cy.get('#nieuwWachtwoord_confirmation').type('User010', { log: false });
+      cy.get('.wachtwoord-wijzigen .btn-primary').click();
+
+      // Verify password change success
+      cy.get('.alert-success').should('be.visible').and('contain', 'Wachtwoord succesvol gewijzigd');
+
+      // Log out
+      cy.get('button[aria-label="Log out"]').click();
+
+      // Log in with new password
+      cy.visit('http://localhost:8000/login');
+      cy.get('input[name="email"]').type('User01@gmail.com');
+      cy.get('input[name="password"]').type('User010', { log: false });
+      cy.get('button[type="submit"]').click();
+
+      // Verify successful login
+      cy.url().should('include', '/dashboard');
+      cy.contains('Dashboard').should('be.visible');
+
     });
 });
